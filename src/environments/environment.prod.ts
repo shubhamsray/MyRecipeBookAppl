a9f07db69df8b0c9fcf5c3197cf82fe04aaa299c
shubham santosh ray <shubhamsray1999@gmail.com>
2024-10-12 00:53:11 +0530

    the file removed

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-// environment.prod.ts (for production mode)
-export const environment = {
-  production: true,
-  googleApiKey: 'AIzaSyDDFmAR0WHqV2rlPLWcad6iHLwNeveKAyQ',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-// environment.ts (for development mode)
-export const environment = {
-  production: false,
-  googleApiKey: 'AIzaSyDDFmAR0WHqV2rlPLWcad6iHLwNeveKAyQ',
-};
